dbd313a040eb54328b23b25815ae68bfab75501e
tianyongchao <51394548+tianyongchao@users.noreply.github.com>
2021-06-04 18:44:56 +0800

    增加了vue，iview

--- src/index.js
+++ src/index.js
@@ -3,12 +3,61 @@
  *   开发环境 webpack ./src/index.js -o ./build/built.js --mode=development
  *  生产环境   webpack ./src/index.js -o ./build/built.js --mode=production
  */
-import data from './data.json'
 import './index.less'
-import print from './print.js'
-function add (x, y) {
-    return x + y
-}
-console.log(add(1,2), 121212121)
-console.log(data)
-print()
+new Vue({
+    el: '#app',
+    data: {
+        visible: false,
+        columns1: [
+                {
+                    title: 'Name',
+                    key: 'name'
+                },
+                {
+                    title: 'Age',
+                    key: 'age'
+                },
+                {
+                    title: 'Address',
+                    key: 'address'
+                }
+            ],
+            data1: [
+                {
+                    name: 'John Brown',
+                    age: 18,
+                    address: 'New York No. 1 Lake Park',
+                    date: '2016-10-03'
+                },
+                {
+                    name: 'Jim Green',
+                    age: 24,
+                    address: 'London No. 1 Lake Park',
+                    date: '2016-10-01'
+                },
+                {
+                    name: 'Joe Black',
+                    age: 30,
+                    address: 'Sydney No. 1 Lake Park',
+                    date: '2016-10-02'
+                },
+                {
+                    name: 'Jon Snow',
+                    age: 26,
+                    address: 'Ottawa No. 2 Lake Park',
+                    date: '2016-10-04'
+                }
+            ]
+    },
+    methods: {
+        show: function () {
+            this.visible = true;
+        },
+        handleChange(data) {
+            console.log(data)
+        }
+    },
+    mounted () {
+        console.log(this.columns1)
+    }
+})
